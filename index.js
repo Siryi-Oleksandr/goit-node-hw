767bc01693af18256689f9481b1ee19c342a7c9d
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-05-03 23:36:48 +0300

    add upadateContact + deleteContact

--- index.js
+++ index.js
@@ -9,10 +9,40 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     case "getById":
       const contact = await contacts.getById(id);
       return console.log(contact);
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case "updateById":
+      const updatedContact = await contacts.updateById(id, {
+        name,
+        email,
+        phone,
+      });
+      return console.log(updatedContact);
+    case "deleteById":
+      const deletedContact = await contacts.deleteById(id);
+      return console.log(deletedContact);
     default:
       return console.log(`not found such command ${action}`);
   }
 };
 
 // invokeAction({ action: "read" });
-invokeAction({ action: "getById", id: "qdggE76Jtbfd9eWJHrssH" });
+// invokeAction({ action: "getById", id: "qdggE76Jtbfd9eWJHrssH" });
+// invokeAction({
+//   action: "add",
+//   name: "Rose Dowson",
+//   email: "dowson@jack.com",
+//   phone: "12-13",
+// });
+// invokeAction({
+//   action: "updateById",
+//   id: "wEi7mZwAZqAXFrIUFhJqI",
+//   name: "Rose Dowson",
+//   email: "dowson@rose.com",
+//   phone: "(294) 987-654123",
+// });
+invokeAction({
+  action: "deleteById",
+  id: "gc0nWBPq1OovUJxlw3TX0",
+});
